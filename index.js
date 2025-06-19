e8076faa213b5eb3c5824b530fdd54b836ff9ef9
dustjua <ggua68647@gmail.com>
2025-06-19 13:41:35 -0600

    Resubida completa del proyecto hyperfilesminter

---
+++ index.js
@@ -0,0 +1,58 @@
+import fs from 'fs';
+import path from 'path';
+import { getStorePath } from './src/functions.js';
+import { App } from './src/app.js';
+import FileExchangeProtocol from "./contract/FileExchangeProtocol.js";
+import FileExchangeContract from "./contract/FileExchangeContract.js";
+import Migration from "./features/migration/index.js";
+
+
+export * from 'trac-peer/src/functions.js';
+
+const RECEIPTS_DIR = path.join(Pear.config.dir, 'receipts'); 
+if (!fs.existsSync(RECEIPTS_DIR)) { 
+    fs.mkdirSync(RECEIPTS_DIR, { recursive: true });
+}
+
+console.log('Storage path:', getStorePath());
+console.log('Receipts path:', RECEIPTS_DIR);
+
+const msb_opts = {
+    bootstrap: '54c2623aa400b769b2837873653014587278fb83fd72e255428f78a4ff7bac87',
+    channel: '00000000000000000000000trac20msb',
+    store_name: getStorePath() + '/t20msb_2'
+};
+
+const peer_opts = {
+    protocol: FileExchangeProtocol,
+    contract: FileExchangeContract,
+    bootstrap: 'c8d69852fe7828709349a68c61c3d88ab9078ec4331e07ebaaf1e8b55e67a287',
+    channel: '00000000000000000000000002trac20',
+    store_name: getStorePath() + '/file-exchange-db',
+    enable_logs: true,
+    enable_txlogs: true,
+    receipts_path: RECEIPTS_DIR
+};
+
+const old_path = getStorePath() + "/trac20_2";
+const new_path = peer_opts.store_name;
+if (false === fs.existsSync(new_path + '/db') && true === fs.existsSync(old_path + '/db/keypair.json')) {
+    fs.mkdirSync(new_path, { recursive: true });
+    fs.mkdirSync(new_path + '/db', { recursive: true });
+    fs.copyFileSync(old_path + '/db/keypair.json', new_path + '/db/keypair.json');
+    fs.rmSync(old_path, { recursive: true, force: true });
+}
+
+export const app = new App(msb_opts, peer_opts, [
+    { name: 'migration', class: Migration }
+]);
+
+await app.start();
+
+console.log("trac-peer node started successfully.");
+console.log("Minter/Owner Address:", app.peer.wallet.publicKey);
+console.log("\nNode is running in interactive mode.");
+console.log("Type '/commands' to see available file exchange options.");
+console.log("========================================================\n");
+
+
